c7a8148638d6da5b35fd9e6104e9be5005896e3a
Colin Quirk <cquirk@uchicago.edu>
2018-02-28 12:12:32 -0600

    More robust, added readmes

--- expdir.py
+++ expdir.py
@@ -6,6 +6,7 @@ import os, sys
 def make_dirs():
     root = os.getcwd()
     folders = {
+        'experiment_code': [],
         'data': ['csv', 'dfs'],
         'exploration': [],
         'figures': ['plots', 'scripts'],
@@ -14,15 +15,19 @@ def make_dirs():
     }
 
     for parent, subs in folders.items():
-        os.mkdir(os.path.join(root, parent))
-        for sub in subs:
-            os.mkdir(os.path.join(root, parent, sub))
+        os.makedirs(os.path.join(root, parent), exist_ok=True)
+        if len(subs) == 0:
+            open(os.path.join(root, parent, 'README.md'), 'a').close()
+        else:
+            for sub in subs:
+                os.makedirs(os.path.join(root, parent, sub), exist_ok=True)
+                open(os.path.join(root, parent, sub, 'README.md'), 'a').close()
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
         make_dirs()
     elif len(sys.argv) == 2:
-        os.mkdir(sys.argv[1])
+        os.makedirs(sys.argv[1], exist_ok=True)
         os.chdir(sys.argv[1])
         make_dirs()
     else:
